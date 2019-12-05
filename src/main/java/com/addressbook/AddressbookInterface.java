ed7cbb682df26647673b8bdd02216f4db9ca871e
Sargam <sargampandey27oct@gmail.com>
2019-12-05 12:40:44 +0530

    added: interface for main method implementation

---
+++ src/main/java/com/addressbook/AddressbookInterface.java
@@ -0,0 +1,9 @@
+package com.addressbook;
+
+public interface AddressbookInterface {
+    public void addPerson();
+    public void editPerson();
+    public void deletePerson();
+    public void sortByName();
+    public void sortByZip();
+}
