69c31bfa279d1577f784b79349343cf7efa1b578
Sargam <sargampandey27oct@gmail.com>
2019-12-05 12:29:51 +0530

    added: getter setter methods for person pojo class

--- src/main/java/com/addressbook/PersonDetailsPojo.java
+++ src/main/java/com/addressbook/PersonDetailsPojo.java
@@ -6,4 +6,44 @@ public class PersonDetailsPojo {
     private String address;
     private String contact;
     AddressDetailsPojo addressDetailsObject;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public AddressDetailsPojo getAddressDetailsObject() {
+        return addressDetailsObject;
+    }
+
+    public void setAddressDetailsObject(AddressDetailsPojo addressDetailsObject) {
+        this.addressDetailsObject = addressDetailsObject;
+    }
 }
