d57daa4b61d655bd2fa418affd972b1e14c315bc
Sargam <sargampandey27oct@gmail.com>
2019-12-05 12:28:59 +0530

    added: person pojo class

---
+++ src/main/java/com/addressbook/PersonDetailsPojo.java
@@ -0,0 +1,9 @@
+package com.addressbook;
+
+public class PersonDetailsPojo {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String contact;
+    AddressDetailsPojo addressDetailsObject;
+}
